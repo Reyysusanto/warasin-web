614e43035c6d09a114402ca0b64ef5c45fe79a6f
Ahmad Mirza <ahmadmirza0585@gmail.com>
2025-04-18 23:04:09 +0700

    entity fixes, migrations, and rollbacks

--- backend/entity/education_entity.go
+++ backend/entity/education_entity.go
@@ -10,7 +10,7 @@ type Education struct {
 	Degree         string    `json:"edu_degree"`
 	Major          string    `json:"edu_major"`
 	Institution    string    `json:"edu_institution"`
-	GraduationYear string    `json:"edu_graduation_year"`
+	GraduationYear string    `gorm:"type:varchar(4)" json:"edu_graduation_year"`
 
 	PsychologID uuid.UUID `gorm:"type:uuid" json:"psy_id"`
 	Psycholog   Psycholog `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
