326e587e6cc8a1ac86795f3fd4a771ba36d4d94e
Ahmad Mirza <ahmadmirza0585@gmail.com>
2025-04-19 14:06:08 +0700

    seeder data for initial display

--- backend/entity/available_slot_entity.go
+++ backend/entity/available_slot_entity.go
@@ -8,8 +8,8 @@ import (
 )
 
 type AvailableSlot struct {
-	ID       uuid.UUID `gorm:"type:uuid:primaryKey" json:"slot_id"`
-	Date     time.Time `json:"slot_date"`
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey" json:"slot_id"`
+	Date     string    `gorm:"type:date" json:"slot_date"`
 	Start    time.Time `json:"slot_start"`
 	End      time.Time `json:"slot_end"`
 	IsBooked bool      `json:"slot_is_booked"`
--- backend/migrations/seed.go
+++ backend/migrations/seed.go
@@ -1,12 +1,93 @@
 package migrations
 
 import (
-	"github.com/Reyysusanto/warasin-web/backend/migrations/seed"
+	"github.com/Reyysusanto/warasin-web/backend/entity"
 	"gorm.io/gorm"
 )
 
 func Seed(db *gorm.DB) error {
-	if err := seed.ListUserSeeder(db); err != nil {
+	err := SeedFromJSON[entity.City](db, "./migrations/json/cities.json", entity.City{}, "Name")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.User](db, "./migrations/json/users.json", entity.User{}, "Email")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.News](db, "./migrations/json/news.json", entity.News{}, "Title", "Date")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.NewsDetail](db, "./migrations/json/news_details.json", entity.NewsDetail{}, "NewsID", "UserID")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.MotivationCategory](db, "./migrations/json/motivation_categories.json", entity.MotivationCategory{}, "Name")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.Motivation](db, "./migrations/json/motivations.json", entity.Motivation{}, "MotivationCategoryID")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.UserMotivation](db, "./migrations/json/user_motivations.json", entity.UserMotivation{}, "UserID", "MotivationID")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.Psycholog](db, "./migrations/json/psychologs.json", entity.Psycholog{}, "Email", "STRNumber")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.LanguageMaster](db, "./migrations/json/language_masters.json", entity.LanguageMaster{}, "Name")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.PsychologLanguage](db, "./migrations/json/psycholog_languages.json", entity.PsychologLanguage{}, "LanguageMasterID", "PsychologID")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.Specialization](db, "./migrations/json/specializations.json", entity.Specialization{}, "Name")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.SpecializationDetail](db, "./migrations/json/specialization_details.json", entity.SpecializationDetail{}, "SpecializationID", "Name")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.PsychologSpecialization](db, "./migrations/json/psycholog_specializations.json", entity.PsychologSpecialization{}, "SpecializationID", "PsychologID")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.Education](db, "./migrations/json/educations.json", entity.Education{}, "PsychologID", "Major", "Degree", "Institution")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.Practice](db, "./migrations/json/practices.json", entity.Practice{}, "PsychologID", "Name")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.PracticeSchedule](db, "./migrations/json/practice_schedules.json", entity.PracticeSchedule{}, "PracticeID", "Day")
+	if err != nil {
+		return err
+	}
+
+	err = SeedFromJSON[entity.AvailableSlot](db, "./migrations/json/available_slots.json", entity.AvailableSlot{}, "PracticeID", "Date")
+	if err != nil {
 		return err
 	}
 
